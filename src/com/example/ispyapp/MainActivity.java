1def0ad1da1e71644bcbd9cda08c4792c1035ba7
Sagarsaurus <Sagarsaurus@github.com>
2014-07-30 21:09:39 -0400

    Can now hit button, take picture, display as thumbnail on main activity.

--- src/com/example/ispyapp/MainActivity.java
+++ src/com/example/ispyapp/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.ispyapp;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends ActionBarActivity {
+	
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+	private ImageView imageview;
+	private Button takePicture;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		this.imageview = (ImageView)this.findViewById(R.id.imageView);
+		takePicture = (Button)findViewById(R.id.takePicture);
+		takePicture.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				dispatchTakePictureIntent();				
+			}
+		});
 	}
 
 	@Override
@@ -30,5 +49,21 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private void dispatchTakePictureIntent() {
+	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+	    }
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	        Bundle extras = data.getExtras();
+	        Bitmap imageBitmap = (Bitmap) extras.get("data");
+	        this.imageview.setImageBitmap(imageBitmap);
+	    }
 	}
 }
